f4366f18de7b2a2b68283299b617cf5ad7d0fab8
Niilop <paakkonenniilo@gmail.com>
2026-03-23 18:48:39 +0200

    delet gitkeep, add minimal streamlit front

---
+++ frontend/app.py
@@ -0,0 +1,32 @@
+# frontend/app.py
+import streamlit as st
+import requests
+
+st.title("DS POC App 🚀")
+
+st.write("Simple frontend connected to FastAPI")
+
+# Inputs
+name = st.text_input("Enter your name")
+task = st.text_input("Enter a task")
+
+# Button
+if st.button("Send Request"):
+    url = "http://127.0.0.1:8000/example/"
+
+    payload = {
+        "name": name,
+        "task": task
+    }
+
+    try:
+        response = requests.post(url, json=payload)
+
+        if response.status_code == 200:
+            result = response.json()["result"]
+            st.success("Response received!")
+            st.write(result)
+        else:
+            st.error(f"Error: {response.status_code}")
+    except Exception as e:
+        st.error(f"Connection error: {e}")
